9217ebed33030708c508f795d95cb24bddeb2eaa
zjohnson5455 <zacharyjohnsonri@gmail.com>
2019-11-06 15:12:39 -0500

    added pre rule change data

--- scraping.R
+++ scraping.R
@@ -1,6 +1,14 @@
 setwd("/Users/zacharyjohnson/Downloads/School/SportsAnalytics/project/NFL-Overtime-Changes/Data")
-winData <- read.csv(file="kickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
-winKickoffs <- winData[(winData$Time == "15:00"), ]
 
-lossData <- read.csv(file="kickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
-lossKickoffs <- lossData[(lossData$Time == "15:00"), ]
+## Post Rule Change: 2012-2016 Seasons
+postRuleWin <- read.csv(file="kickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+postRuleWinKickoffs <- postRuleWin[(postRuleWin$Time == "15:00"), ]
+
+postRuleLoss <- read.csv(file="kickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+postRuleLossKickoffs <- postRuleLoss[(postRuleLoss$Time == "15:00"), ]
+
+## Pre Rule Change: 2007-2011 Seasons
+
+preRuleWin <- read.csv(file="07-11KickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+preRuleLoss <- read.csv(file="07-11KickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+
