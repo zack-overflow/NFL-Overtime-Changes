4b4cf0919c53b31f8ab26a3b06eb57d9fec0d36c
zackg22 <Zack.Gottesman@likewise.com>
2019-11-06 23:37:27 -0500

    updated file paths

--- scraping.R
+++ scraping.R
@@ -1,14 +1,11 @@
-setwd("/Users/zacharyjohnson/Downloads/School/SportsAnalytics/project/NFL-Overtime-Changes/Data")
-
 ## Post Rule Change: 2012-2016 Seasons
-postRuleWin <- read.csv(file="kickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+postRuleWin <- read.csv(file="Data/kickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 postRuleWinKickoffs <- postRuleWin[(postRuleWin$Time == "15:00"), ]
 
-postRuleLoss <- read.csv(file="kickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+postRuleLoss <- read.csv(file="Data/kickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 postRuleLossKickoffs <- postRuleLoss[(postRuleLoss$Time == "15:00"), ]
 
 ## Pre Rule Change: 2007-2011 Seasons
 
-preRuleWin <- read.csv(file="07-11KickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
-preRuleLoss <- read.csv(file="07-11KickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
-
+preRuleWin <- read.csv(file="Data/07-11KickoffWon.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+preRuleLoss <- read.csv(file="Data/07-11KickoffLost.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
